661d7e15b18bc4872ee4420f54561c47c3a96b2b
helyezerteofilo <helyezerteofilo2@hotmail.com>
2023-09-12 00:37:49 -0300

    feat: organization commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './pages/HomePage';
 import reportWebVitals from './reportWebVitals';
 import  {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import ProductDetail from './pages/ProductDetail';
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Card from './components/Card';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import './css/App.css';
+import Card from '../components/Card';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import '../css/HomePage.css';
 import { Row } from 'antd';
-import CarouselApp from './components/Carousel';
+import CarouselApp from '../components/Carousel';
 
 function App() {
   const [products, setProdutos] = useState([]);
@@ -19,14 +19,14 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Header/>
       <CarouselApp products={products}></CarouselApp>
       <Row gutter={[16, 16]}>
       {products.map((product) => (
         <Card key={product.id} produto={product} />
       ))}
     </Row>
-      <Footer />
+      <Footer/>
     </div>
   );
 }
